6e66644c5a65ed3b54880de53f830005b5ebfaf5
wachinaba <wachinaba@gmail.com>
2024-07-08 20:25:40 +0900

    fix compile error in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,9 @@
 
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Login() {
+function LoginButton() {
     const searchParams = useSearchParams();
     // ログイン後に遷移するページ
     const callbackUrl = searchParams.get("callbackUrl") || "/profile";
@@ -12,5 +13,13 @@ export default function Login() {
         <button onClick={() => signIn("google", { callbackUrl })}>
             Login With Google
         </button>
+    );
+}
+
+export default function Login() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <LoginButton />
+        </Suspense>
     );
 }
