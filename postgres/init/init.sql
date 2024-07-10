85502d7722640a3458dc808d13ce20e64d629fcd
wachinaba <wachinaba@gmail.com>
2024-07-10 17:57:20 +0900

    change database initializer

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,36 +1,17 @@
-CREATE TABLE verification_token (
-    identifier TEXT NOT NULL,
-    expires TIMESTAMPTZ NOT NULL,
-    token TEXT NOT NULL,
-    PRIMARY KEY (identifier, token)
-);
-CREATE TABLE accounts (
-    id SERIAL,
-    "userId" INTEGER NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    provider VARCHAR(255) NOT NULL,
-    "providerAccountId" VARCHAR(255) NOT NULL,
-    refresh_token TEXT,
-    access_token TEXT,
-    expires_at BIGINT,
-    id_token TEXT,
-    scope TEXT,
-    session_state TEXT,
-    token_type TEXT,
-    PRIMARY KEY (id)
-);
-CREATE TABLE sessions (
-    id SERIAL,
-    "userId" INTEGER NOT NULL,
-    expires TIMESTAMPTZ NOT NULL,
-    "sessionToken" VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
 CREATE TABLE users (
-    id SERIAL,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    "emailVerified" TIMESTAMPTZ,
-    image TEXT,
-    PRIMARY KEY (id)
+    id TEXT NOT NULL PRIMARY KEY,
+    display_name TEXT,
+    icon_path TEXT,
+    current_task TEXT,
+    current_task_time TIMESTAMP
+);
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    task_name TEXT NOT NULL,
+    deadline DATE,
+    total_set INTEGER NOT NULL,
+    current_set INTEGER NOT NULL,
+    is_complete BOOLEAN NOT NULL Default false,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
