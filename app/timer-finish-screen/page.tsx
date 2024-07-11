95cb1f2eb458a94014838f63d1df2f8dc4ac95b8
yusei_yamada <yamayuh13@icloud.com>
2024-07-11 19:24:40 +0900

    タスクのコンプリート

---
+++ app/timer-finish-screen/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import styles from "./TimerFinishScreen.module.css";
+
+export default function TimerFinishScreen() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.Header}>
+        <h1 className={styles.HeaderTitle}>いつやるの？</h1>
+        <button className={styles.HeaderButton}>設定ボタン</button>
+      </div>
+      <div className={styles.FinishTexts}>
+        <h2 className={styles.TaskFinishText}>休憩が終了しました！</h2>
+        <h2 className={styles.TaskChangeText}>タスクを変更しますか？</h2>
+      </div>
+      <div className={styles.ControlNextTaskFrame}>
+        <button className={styles.TaskContinue}>続ける</button>
+        <p className={styles.TextOR}>or</p>
+        <svg className={styles.NextTasks}>
+        <rect width="300" height="1000" style={{ fill: 'gray', strokeWidth: 3, stroke: 'black' }} />
+        </svg>
+      </div>
+      <div className={styles.Footer}>
+        <button className={styles.ButtonTaskManegement}>タスク画面</button>
+      </div>
+    </main>
+  );
+}
--- app/timer-working-screen/page.tsx
+++ app/timer-working-screen/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import styles from "./TimerStartScreen.module.css";
+import styles from "./TimerWorkingScreen.module.css";
 
-export default function TimerStartScreen() {
+export default function TimerWorkingScreen() {
   return (
     <main className={styles.main}>
       <div className={styles.Header}>
@@ -16,11 +16,7 @@ export default function TimerStartScreen() {
         <h2 className={styles.TimeGage}>時間ゲージ</h2>
       </div>
       <div className={styles.TimeButtons}>
-        <button className={styles.ButtonPlayBack}>再生</button>
         <button className={styles.ButtonPause}>一時停止ボタン</button>
-      </div>
-      <div className={styles.NavigateTaskButton}>
-        <button className={styles.ButtonTaskManegement}>タスク画面</button>
       </div>
     </main>
   );
