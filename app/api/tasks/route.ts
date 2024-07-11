e919f7d0c95b9b55b5e083d790c6d17537027764
wachinaba <wachinaba@gmail.com>
2024-07-11 19:46:37 +0900

    add authentication for add-task request

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -16,7 +16,25 @@ export async function GET(req: Request) {
 }
 
 export async function POST(req: Request) {
+    const session = await getServerSession(authOptions);
+    // check if session is not null
+    if (!session) {
+        return new Response(JSON.stringify({ error: 'Unauthorized: session does not exist' }), { status: 401 });
+    }
+    if (!session.user) {
+        return new Response(JSON.stringify({ error: 'Unauthorized: session user does not exist' }), { status: 401 });
+    }
+    if (!("id" in session.user)) {
+        return new Response(JSON.stringify({ error: 'Unauthorized: session user does not have an id' }), { status: 401 });
+    }
+    const session_user_id = session.user.id;
     const { user_id, task_name, deadline, total_set, current_set, is_complete } = await req.json();
+
+    // check if user_id is same as session.user.id
+    if (user_id !== session_user_id) {
+        return new Response(JSON.stringify({ error: 'Unauthorized: the requested user does not match the session user' }), { status: 401 });
+    }
+
     try {
         const { rows } = await query('INSERT INTO tasks (user_id, task_name, deadline, total_set, current_set, is_complete) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [user_id, task_name, deadline, total_set, current_set, is_complete]);
         return new Response(JSON.stringify(rows[0]), { status: 201 });
