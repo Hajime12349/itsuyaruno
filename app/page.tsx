e188db0b3844dd4777374e03bdc9b7ae4b93d0fd
Hajime12349 <qqwwkami@gmail.com>
2024-07-12 22:01:16 +0900

    improve homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1>Hello Wor!!</h1>
+        <h1>This is HomePage. This page will be delete.</h1>
+        The URL to app is as follows (dev only).<br />
+        <a href="http://localhost:3000/timer-start-screen">timer-start-screen</a>
       </div>
     </main>
   );
