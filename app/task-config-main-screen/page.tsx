edfe950422c029a9c2691325f247e0cd08ec4a59
Hajime12349 <qqwwkami@gmail.com>
2024-07-12 21:10:48 +0900

    attach change and fix bugs

--- app/task-config-main-screen/page.tsx
+++ app/task-config-main-screen/page.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image";
 import React from 'react';
 import styles from './page.module.css';  
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <main>
       <div>
-        <div className={styles.flexContainer}>
-          <h1 className={styles.title}>いつやるの？</h1>
-          <Image src="/config.png" alt="設定" width={30} height={30} />
-        </div>
-      <input type="text" placeholder="検索" className={styles.search} />
+          <Header />
+          <input type="text" placeholder="検索" className={styles.search} />
       </div>
     </main>
   );
