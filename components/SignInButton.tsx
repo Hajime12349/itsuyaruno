a1447f72eb8654fcfc780b3f5504981fa7e7ed23
wachinaba <wachinaba@gmail.com>
2024-07-13 23:28:27 +0900

    fix callback url of signin button

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 
 export default function SignInButton() {
     return (
-        <button onClick={() => signIn("google")}>
+        <button onClick={() => signIn("google", { callbackUrl: "/loggedin" })}>
             Login With Google
         </button>
     );
