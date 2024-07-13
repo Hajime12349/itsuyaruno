328d77b9bb01708422765431e0816ea7321222fb
Hajime12349 <qqwwkami@gmail.com>
2024-07-13 18:07:28 +0900

    bugfix taskpanel

--- app/experiment/components/task-panel/page.tsx
+++ app/experiment/components/task-panel/page.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import TaskPanel from "@/components/TaskPanel";
 
 //<TaskPanel title="title" subtitle="sub" progress="before start" dueIn="3day"/>
+//<TaskPanel taskName="title" currentSet={1} totalSet={2} deadline={3}/>
       
 export default function TestComponets() {
   return (
     <main>
       <div>
-        <TaskPanel taskName="title" currentSet={1} totalSet={2} deadline={3}/>
       </div>
     </main>
   );
