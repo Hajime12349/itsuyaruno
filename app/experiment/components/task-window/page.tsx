072dd2909403ac0f39e5cafa1e760cd9dfa97949
Yuta <yuta.shibasaki.ah@gmail.com>
2024-07-13 13:39:31 +0900

    add 'react-modal' and minor corrections

--- app/experiment/components/task-window/page.tsx
+++ app/experiment/components/task-window/page.tsx
@@ -1,14 +1,14 @@
+"use client"
 import Image from "next/image";
-import React from 'react';
+import React, { useState } from 'react';
 import TaskWindow from "@/components/addTaskWindow";
 
-//<TaskPanel title="title" subtitle="sub" progress="before start" dueIn="3day"/>
-      
 export default function TestComponets() {
+
   return (
     <main>
       <div>
-        <TaskWindow/>
+        <TaskWindow />
       </div>
     </main>
   );
--- components/addTaskWindow.tsx
+++ components/addTaskWindow.tsx
@@ -4,81 +4,87 @@ import { useForm } from 'react-hook-form'
 import styles from './taskWindow.module.css'
 import { Task, User } from '@/lib/entity'
 import { createTask } from '@/lib/db_api_wrapper'
+import Modal from "react-modal";
+
+Modal.setAppElement(".App");
+
+
 
 const page = () => {
-    const { register, handleSubmit, setValue, getValues } = useForm()
-    
-    // 新しい関数を定義
-    const handleTaskData = (taskData: { task_name: string, total_set: number, deadline: string, current_set: number, is_complete: boolean }) => {
-        // ここでtaskDataを使って何かを行う
-        createTask(taskData).then(() => {
-            console.log("タスクを追加しました");
-        }).catch((error) => {
-            console.error("タスクの追加に失敗しました", error);
-        });
-    }
+  //フォームの値を管理するためのステート
+  const { register, handleSubmit, setValue, getValues } = useForm()
+  // 詳細設定の表示状態を管理するためのステート
+  const [showDetails, setShowDetails] = useState(false);
+  //モーダルの表示状態を管理するためのステート
+  const [ modalIsOpen, setIsOpen ] = useState(false);
 
-    //クリック時のアクション
-    const onSubmit = (data: any) => {
-        let { task_name, total_set, deadline } = data;
-        let current_set = total_set; // current_setをtotal_setと同じに設定
-        let is_complete = false; // is_completeをfalseに設定
+  
+  //クリック時のアクション
+  const onSubmit = (data: any) => {
+     let { task_name, total_set, deadline } = data;
+     let current_set = total_set; // current_setをtotal_setと同じに設定
+    let is_complete = false; // is_completeをfalseに設定
         
-        if (!task_name || !total_set || !deadline) {
-            alert("タイトルとセット数と期限を入力してください");
-            return;
-        }
-        
-        const taskData = { task_name, total_set, deadline, current_set, is_complete };
-        handleTaskData(taskData); // 新しい関数にデータを渡す
+    if (!task_name || !total_set || !deadline) {
+      alert("タイトルとセット数と期限を入力してください");
+      return;
     }
-    const today = new Date().toISOString().split('T')[0];
+    const taskData = { task_name, total_set, deadline, current_set, is_complete };
+    handleTaskData(taskData); // 受け渡し用関数にデータを渡す
+  }
+  // 受け渡し用関数
+  const handleTaskData = (taskData: { task_name: string, total_set: number, deadline: string, current_set: number, is_complete: boolean }) => {
+    createTask(taskData).then(() => {
+      console.log("タスクを追加しました");
+    }).catch((error) => {
+      console.error("タスクの追加に失敗しました", error);
+    });
+  }
 
-    // 詳細設定の表示状態を管理するためのステート
-    const [showDetails, setShowDetails] = useState(false);
+  // 今日の日付を取得
+  const today = new Date().toISOString().split('T')[0];
 
-
-
-    // totalSetをランダムに設定する関数
-    const setRandomTotalSet = () => {
-        const randomValue = Math.floor(Math.random() * 3) + 1;
-        setValue('total_set', randomValue);
-    };
-
-    return (
-    <div>
-        <div className={styles.header}>
-            <h1>タスク追加</h1>
-            <button className={styles.closeButton}>×</button>
-        </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <p>タイトル</p>
-          <input id="task_name" {...register('task_name')}/>
-        </div>
-        <div>
-          <p>セット数</p>
-          <button type="button" onClick={setRandomTotalSet}>自動</button>
-          <input id="total_set" type="number" min="1" step="1" defaultValue={1} {...register('total_set')}/>
-        </div>
-
-        <div>
-          <button type="button" onClick={() => setShowDetails(!showDetails)}>詳細設定</button>
-          //詳細設定の表示状態を管理するためのステート
-          {showDetails && (
-            <div>
-              <p>期限</p>
-              <input type="date" defaultValue={today} {...register('deadline')} />
-            </div>
-          )}
-        </div>
-
-            <button type="submit">追加</button>
-        </form>
-    </div>
-    );
+  // totalSetをランダムに設定する関数
+  const setRandomTotalSet = () => {
+      const randomValue = Math.floor(Math.random() * 3) + 1;
+      setValue('total_set', randomValue);
+  };
 
 
+
+  return (
+  <div className="App">
+    <Modal isOpen={modalIsOpen}>
+      <div className={styles.header}>
+          <h1>タスク追加</h1>
+          <button className={styles.closeButton} onClick={() => setIsOpen(false)}>×</button>
+      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <p>タイトル</p>
+        <input id="task_name" {...register('task_name')}/>
+      </div>
+      <div>
+        <p>セット数</p>
+        <button type="button" onClick={setRandomTotalSet}>自動</button>
+        <input id="total_set" type="number" min="1" step="1" defaultValue={1} {...register('total_set')}/>
+      </div>
+
+      <div>
+        <button type="button" onClick={() => setShowDetails(!showDetails)}>詳細設定</button>
+        {showDetails && (
+          <div>
+            <p>期限</p>
+            <input type="date" defaultValue={today} {...register('deadline')} />
+          </div>
+        )}
+      </div>
+
+          <button type="submit">追加</button>
+      </form>
+      </Modal>
+  </div>
+  );
 };
 
 export default page;
