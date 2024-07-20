721c2a2a39ebdee65805fde2754f93bfb460590f
wachinaba <wachinaba@gmail.com>
2024-07-20 18:03:51 +0900

    fix TagInput component

--- components/TagInput.tsx
+++ components/TagInput.tsx
@@ -1,42 +1,44 @@
 import { Autocomplete, TextField, Chip } from "@mui/material";
-import { Task, Tag } from "@/lib/entity";
-import { Controller, useFormContext } from "react-hook-form";
+import { Tag } from "@/lib/entity";
 
-const TagInput = () => {
-    const { control } = useFormContext();
+interface TagInputProps {
+    defaultTags?: Tag[];
+    onChange: (tags: Tag[]) => void;
+    value?: Tag[];
+    onBlur?: () => void;
+    ref?: React.Ref<HTMLInputElement>;
+    style?: React.CSSProperties;
+}
+
+export default function TagInput({ onChange, style, value, onBlur, ref }: TagInputProps) {
     const tagOptions: Tag[] = [{ tag_name: "tag1" }, { tag_name: "tag2" }, { tag_name: "tag3" }];
 
     return (
-        <Controller
-            name="tags"
-            control={control}
-            render={({ field, formState: { errors } }) => (
-                <Autocomplete
-                    multiple
-                    id="tags-filled"
-                    options={tagOptions.map((tag) => tag.tag_name)}
-                    defaultValue={[]}
-                    freeSolo
-                    renderTags={(value: readonly string[], getTagProps) =>
-                        value.map((option: string, index: number) => {
-                            const { key, ...tagProps } = getTagProps({ index });
-                            return (
-                                <Chip variant="outlined" label={option} key={key} {...tagProps} />
-                            );
-                        })
-                    }
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            variant="filled"
-                            placeholder="タグを追加"
-                            label="タグ"
-                            inputRef={field.ref}
-                            value={field.value}
-                            onBlur={field.onBlur}
-                            onChange={field.onChange}
-                        />
-                    )}
+        <Autocomplete
+            multiple
+            options={tagOptions.map((tag) => tag.tag_name)}
+            defaultValue={value?.map((tag) => tag.tag_name) ?? []}
+            value={value?.map((tag) => tag.tag_name) ?? []}
+            onChange={(e, newValue) => {
+                onChange(newValue.map((tag) => ({ tag_name: tag })));
+            }}
+            onBlur={onBlur}
+            freeSolo
+            renderTags={(value: readonly string[], getTagProps) =>
+                value.map((option: string, index: number) => {
+                    const { key, ...tagProps } = getTagProps({ index });
+                    return (
+                        <Chip variant="outlined" label={option} key={key} {...tagProps} />
+                    );
+                })
+            }
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    placeholder="タグを追加、Enterで新規作成"
+                    label="タグ"
+                    ref={ref}
+                    style={style}
                 />
             )}
         />
