37ffb4339d1830af746f08d445a3a4b56ac4dc19
wachinaba <wachinaba@gmail.com>
2024-07-20 17:09:42 +0900

    Change using react-hook-form

--- components/AddTaskPanel.tsx
+++ components/AddTaskPanel.tsx
@@ -6,121 +6,153 @@ import TextField from '@mui/material/TextField';
 import Collapse from '@mui/material/Collapse';
 import { useState } from 'react';
 import { Task } from '@/lib/entity';
+import { useForm, Controller } from 'react-hook-form';
 
 interface AddTaskPanelProps {
     onAddTask: (task: Task) => void;
 }
 
+interface TaskFormInputs {
+    taskName: string;
+    taskTag: string;
+    totalSet: number;
+    currentSet: number;
+    taskDeadline: string | null;
+}
+
 export default function AddTaskPanel({ onAddTask }: AddTaskPanelProps) {
     const [expanded, setExpanded] = useState(false);
-    const [taskName, setTaskName] = useState('');
-    const [taskTag, setTaskTag] = useState('');
-    const [totalSet, setTotalSet] = useState<number | string>('');
-    const [currentSet, setCurrentSet] = useState<number | string>(0);
-    const [taskDeadline, setTaskDeadline] = useState<string | null>(null);
+    const { handleSubmit, control, reset } = useForm<TaskFormInputs>();
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
 
     const handleCancel = () => {
-        setTaskName('');
-        setTaskTag('');
-        setTotalSet('');
-        setCurrentSet(0);
-        setTaskDeadline(null);
+        reset();
         setExpanded(false);
     };
 
-    const handleSave = () => {
+    const onSubmit = (data: TaskFormInputs) => {
         const newTask: Task = {
-            task_name: taskName,
-            total_set: typeof totalSet === 'string' ? parseInt(totalSet, 10) : totalSet,
-            current_set: typeof currentSet === 'string' ? parseInt(currentSet, 10) : currentSet,
+            task_name: data.taskName,
+            total_set: data.totalSet,
+            current_set: data.currentSet,
             is_complete: false,
-            deadline: taskDeadline ? new Date(taskDeadline).toISOString() : undefined,
-            tags: taskTag ? [{ tag_name: taskTag }] : undefined,
+            deadline: data.taskDeadline ? new Date(data.taskDeadline).toISOString() : undefined,
+            tags: data.taskTag ? [{ tag_name: data.taskTag }] : undefined,
         };
         onAddTask(newTask);
         handleCancel();
     };
 
-
     return (
         <Card>
-            <Collapse in={expanded}>
-                <CardContent>
-                    <TextField
-                        label="Task Name"
-                        value={taskName}
-                        onChange={(e) => setTaskName(e.target.value)}
-                        fullWidth
-                    />
-                    <TextField
-                        label="Tag"
-                        value={taskTag}
-                        onChange={(e) => setTaskTag(e.target.value)}
-                        fullWidth
-                        style={{ marginTop: '16px' }}
-                    />
-                    <TextField
-                        label="Total Sets"
-                        value={totalSet}
-                        onChange={(e) => setTotalSet(e.target.value)}
-                        type="number"
-                        fullWidth
-                        style={{ marginTop: '16px' }}
-                    />
-                    <TextField
-                        label="Current Set"
-                        value={currentSet}
-                        onChange={(e) => setCurrentSet(e.target.value)}
-                        type="number"
-                        fullWidth
-                        style={{ marginTop: '16px' }}
-                    />
-                    <TextField
-                        label="Deadline"
-                        value={taskDeadline}
-                        onChange={(e) => setTaskDeadline(e.target.value)}
-                        type="date"
-                        InputLabelProps={{ shrink: true }}
-                        fullWidth
-                        style={{ marginTop: '16px' }}
-                    />
-                </CardContent>
-            </Collapse>
-            <CardActions>
-                <Collapse in={expanded} orientation="horizontal" style={{ width: '100%' }}>
-                    <Button
-                        onClick={handleCancel}
-                        color="secondary"
-                        fullWidth
-                        style={{ marginTop: '8px' }}
-                    >
-                        Cancel
-                    </Button>
-                </Collapse>
-                <Collapse in={expanded} orientation="horizontal" style={{ width: '100%' }}>
-                    <Button
-                        onClick={handleSave}
-                        color="primary"
-                        fullWidth
-                        style={{
-                            marginTop: '8px',
-                            backgroundColor: '#3f51b5',
-                            color: 'white'
-                        }}
-                    >
-                        Save
-                    </Button>
-                </Collapse>
-                <Collapse in={!expanded} orientation="horizontal" style={{ width: '100%' }}>
+            <Collapse in={!expanded}>
+                <CardActions>
                     <Button onClick={handleExpandClick} fullWidth key="add-task-button">
                         +
                     </Button>
-                </Collapse>
-            </CardActions>
+                </CardActions>
+            </Collapse>
+            <Collapse in={expanded}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <CardContent>
+                        <Controller
+                            name="taskName"
+                            control={control}
+                            defaultValue=""
+                            rules={{ required: "タスク名を入力してください" }}
+                            render={({ field, formState: { errors } }) => (
+                                <TextField
+                                    {...field}
+                                    label="Task Name"
+                                    fullWidth
+                                    error={!!errors.taskName}
+                                    helperText={errors.taskName?.message}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="taskTag"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    label="Tag"
+                                    fullWidth
+                                    style={{ marginTop: '16px' }}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="totalSet"
+                            control={control}
+                            defaultValue={0}
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    label="Total Sets"
+                                    type="number"
+                                    fullWidth
+                                    style={{ marginTop: '16px' }}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="currentSet"
+                            control={control}
+                            defaultValue={0}
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    label="Current Set"
+                                    type="number"
+                                    fullWidth
+                                    style={{ marginTop: '16px' }}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="taskDeadline"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    label="Deadline"
+                                    type="date"
+                                    InputLabelProps={{ shrink: true }}
+                                    fullWidth
+                                    style={{ marginTop: '16px' }}
+                                />
+                            )}
+                        />
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            onClick={handleCancel}
+                            color="secondary"
+                            sx={{ width: '25%' }}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            type="submit"
+                            color="primary"
+                            fullWidth
+                            sx={{
+                                backgroundColor: '#3f51b5',
+                                color: 'white',
+                                width: '75%'
+                            }}
+                        >
+                            Save
+                        </Button>
+                    </CardActions>
+                </form>
+            </Collapse>
         </Card>
     );
 }
