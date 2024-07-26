74983745177b78f055bef17bbbbe4b726bcf9e5c
wachinaba <wachinaba@gmail.com>
2024-07-27 03:13:37 +0900

    add db test page

---
+++ app/experiment/db/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { db, Task } from "@/lib/kysely"
+import { useState, useEffect } from "react"
+
+export default function DBPage() {
+    const [tasks, setTasks] = useState<Task[]>([])
+    useEffect(() => {
+        db.selectFrom('tasks').selectAll().execute().then(setTasks)
+    }, [])
+    return (
+        <div>
+            <h1>Tasks</h1>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>{task.task_name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
