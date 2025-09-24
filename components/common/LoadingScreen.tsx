c0835e1f2ccb5ee01afc5a652e511085966ffe88
Hajime12349 <qhqiwmwa@gmail.com>
2025-09-24 23:06:38 +0900

    add: loading screen

---
+++ components/common/LoadingScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./LoadingScreen.module.css";
+
+const LoadingScreen: React.FC = () => {
+    return <div className={styles.container}>Loading...</div>;
+};
+
+export default LoadingScreen;
