f7c2357d7508b16b20313b24714cdd7eec4e9882
Hajime12349 <qhqiwmwa@gmail.com>
2025-11-18 21:54:32 +0900

    fix: linter autfix settings

--- domain/users/valueObjects/__tests__/UserId.test.ts
+++ domain/users/valueObjects/__tests__/UserId.test.ts
@@ -1,11 +1,10 @@
-import { describe,expect, it} from "@jest/globals";
-import { UserId } from "../UserId";
+import { describe, expect, it } from '@jest/globals';
+import { UserId } from '../UserId';
 
-describe("UserId", () => {
-  it ("ユーザーIDを生成できる", () => {
-    const userId = UserId.create("user-123");
+describe('UserId', () => {
+  it('ユーザーIDを生成できる', () => {
+    const userId = UserId.create('user-123');
     expect(userId).toBeInstanceOf(UserId);
-    expect(userId.value).toBe("user-123");
+    expect(userId.value).toBe('user-123');
   });
 });
- 
