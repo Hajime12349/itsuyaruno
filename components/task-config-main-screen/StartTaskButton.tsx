5f7e48af7f5de0309e8a83bf8046c79c95c38bdb
Hajime <qqwwkami@gmail.com>
2026-03-21 15:46:30 +0900

    fix: unknown

---
+++ components/task-config-main-screen/StartTaskButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import type { ITaskDataModel as Task } from "@/application/tasks/TaskDataModelMapper";
+import styles from "./StartTaskButton.module.css";
+
+interface StartTaskButtonProps {
+  task: Task;
+  onStart?: (task: Task) => void;
+}
+
+const StartTaskButton: React.FC<StartTaskButtonProps> = ({ task, onStart }) => {
+  function handleClick() {
+    if (onStart) {
+      onStart(task);
+    }
+  }
+
+  // TSXを返す
+  // TODO: ボタンのスタイルを設定する
+  return <button className={styles.StartButton} onClick={handleClick}>開始</button>;
+};
+
+export default StartTaskButton;
